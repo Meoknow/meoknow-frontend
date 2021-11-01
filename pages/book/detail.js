7c07b4dfcbc922ca2318fa60e5a4b9707a1ac4e5
Mustingu <1900016627@pku.edu.cn>
2021-11-01 13:44:15 +0800

    猫图鉴分类

--- pages/book/detail.js
+++ pages/book/detail.js
@@ -1,66 +1,47 @@
-// pages/book/detail.js
 Page({
-
-    /**
-     * 页面的初始数据
-     */
-    data: {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
-    }
+  data: {
+      category: [
+          {name:'A',id:'1'},
+          {name:'B',id:'2'},
+          {name:'C',id:'3'},
+          {name:'D',id:'4'},
+          {name:'E',id:'5'},
+          {name:'F',id:'6'}
+      ],
+      detail:[],
+      curIndex: 0,
+      isScroll: false,
+      toView: 'guowei'
+  },
+  onReady(){
+      var self = this;
+      wx.request({
+          url:'http://www.gdfengshuo.com/api/wx/cate-detail.txt',
+          success(res){
+              self.setData({
+                  detail : res.data
+              })
+          }
+      });
+      
+  },
+  switchTab(e){
+    const self = this;
+    this.setData({
+      isScroll: true
+    })
+    setTimeout(function(){
+      self.setData({
+        toView: e.target.dataset.id,
+        curIndex: e.target.dataset.index
+      })
+    },0)
+    setTimeout(function () {
+      self.setData({
+        isScroll: false
+      })
+    },1)
+      
+  }
+  
 })
